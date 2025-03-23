9b0bc9fd85ab0c5afd4684ab5574b554f0bd179b
Kansha M <144353246+MKansha@users.noreply.github.com>
2025-03-23 14:20:50 +0530

    Update axiosConfig.jsx

--- Frontend/src/utils/axiosConfig.jsx
+++ Frontend/src/utils/axiosConfig.jsx
@@ -1,6 +1,6 @@
 import axios from "axios";
 
-axios.defaults.baseURL = "http://127.0.0.1:8000/api";
+axios.defaults.baseURL = "https://webelievelogistics.org/api";
 axios.defaults.withCredentials = true; // Send cookies with requests
 
 export default axios;
